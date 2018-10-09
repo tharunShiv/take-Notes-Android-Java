c5cad94a437e322cc4c6804812e65f08c8a23ed0
tharunShiv <tharun.rocky14@gmail.com>
2018-10-09 20:48:03 +0530

    able to add notes without sharedpreferences

--- app/src/main/java/soon/domain/one/take_notes/MainActivity.java
+++ app/src/main/java/soon/domain/one/take_notes/MainActivity.java
@@ -3,6 +3,9 @@ package soon.domain.one.take_notes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,7 +14,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    static ArrayAdapter arrayAdapter;
     // to access this at the next intent
     static ArrayList<String> notes = new ArrayList<>();
 
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         // setting a listview and the adapter
         ListView listView = findViewById(R.id.listView);
         notes.add("Example Note");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(arrayAdapter);
 
         // move to second activity on click
@@ -36,5 +39,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.add_note) {
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/soon/domain/one/take_notes/NoteEditorActivity.java
+++ app/src/main/java/soon/domain/one/take_notes/NoteEditorActivity.java
@@ -3,9 +3,13 @@ package soon.domain.one.take_notes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 public class NoteEditorActivity extends AppCompatActivity {
+
+    int noteId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,19 +17,42 @@ public class NoteEditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note_editor);
 
         // access the edit text
-        EditText editText = findViewById(R.id.editText);
+        final EditText editText = findViewById(R.id.editText);
 
         // get the extra
         Intent intent = getIntent();
         // -1 to denote some error
-        int noteId = intent.getIntExtra("noteId", -1);
+        noteId = intent.getIntExtra("noteId", -1);
 
         // no error
         if (noteId != -1) {
             // display the note
             editText.setText(MainActivity.notes.get(noteId));
+        } else {
+            // when we add new note
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() - 1;
         }
 
+        // to save when changes occur
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // this is the place we want to to our save stuff
+                // lets update the array
+                MainActivity.notes.set(noteId, String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
