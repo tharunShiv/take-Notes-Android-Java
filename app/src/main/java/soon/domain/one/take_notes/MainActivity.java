dd8c5fef12f45beb9ceadb11b03fab7a186e22e0
tharunShiv <tharun.rocky14@gmail.com>
2018-10-09 19:09:16 +0530

    created the second activity and intent switch

---
+++ app/src/main/java/soon/domain/one/take_notes/MainActivity.java
@@ -0,0 +1,39 @@
+package soon.domain.one.take_notes;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // setting a listview and the adapter
+        ListView listView = findViewById(R.id.listView);
+        notes.add("Example Note");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
+
+        // move to second activity on click
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+                intent.putExtra("noteId", i);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/soon/domain/one/take_notes/NoteEditorActivity.java
@@ -0,0 +1,14 @@
+package soon.domain.one.take_notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+    }
+}
